1c55c81c583cf0f910b1e800425328cf405fa0de
Dagdon <dagdonjephthah@gmail.com>
2025-03-20 11:00:25 +0100

    db config

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,13 +1,13 @@
 // backend/config/db.js
 import dotenv from 'dotenv';
- 
+
 // Load environment variables from .env file
 dotenv.config();
 
 //  Export database configuration
 export default {
-	host: process.env.DB_HOST,      // Database host
-	user: process.env.DB_USER,      // Database user
-	password: process.env.DB_PASSWORD, // Database password
-	database: process.env.DB_NAME,  // Database name
+  host: process.env.DB_HOST,      // Database host
+  user: process.env.DB_USER,      // Database user
+  password: process.env.DB_PASSWORD, // Database password
+  database: process.env.DB_NAME,  // Database name
 };
