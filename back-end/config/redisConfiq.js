3a6355e607c06774f9be8075df55c213ebbbc2d2
Dagdon <dagdonjephthah@gmail.com>
2025-03-27 14:12:25 +0100

    redis config

---
+++ back-end/config/redisConfiq.js
@@ -0,0 +1,8 @@
+const redisConfig = {
+  socket: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+  },
+  password: process.env.REDIS_PASSWORD,
+};
+const redisClient = redis.createClient(redisConfig);
